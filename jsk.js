b34131d757785d01f4bbf26f6ff05710259a876e
REBBOUH Amine <96534508+amineREBBOUH@users.noreply.github.com>
2022-06-09 21:26:43 +0100

    login

---
+++ jsk.js
@@ -0,0 +1,15 @@
+
+
+function create(){
+    var create=document.getElementById("div1");
+    create.style.display = "none";
+    var log=document.getElementById("div2");
+    log.style.display = "flex";
+}
+function login(){
+    var log=document.getElementById("div2");
+    log.style.display = "none";
+    var create=document.getElementById("div1");
+    create.style.display = "flex";
+}
+
